4d1f0f0bf2a5278fb6b3706bef1bb1842b6fa1b8
SazanaNET <34138845+SazanaNET@users.noreply.github.com>
2019-05-22 22:44:36 +0300

    1 занятие завершено!

--- sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
@@ -2,29 +2,12 @@ package ru.stqa.pft.sandbox;
 
 public class FirstProgram {
   public static void main(String[] args) {
-    hello("world");
-    hello("user");
-    hello("Алексей");
 
-    double l = 5;
-    System.out.println("Плозадь квадрата со стороной " + l + " = " + area(l));
+    Square s = new Square(9);
+    System.out.println("Плозадь квадрата со стороной " + s.l + " = " + s.area());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    Rectangle r = new Rectangle(8,8);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
   }
 
-
-
-  public static void hello(String somebody) {
-    System.out.println("Hello, " + somebody + "!");
-  }
-
-  public static double area(double len) {
-    return len * len;
-  }
-
-  public static double area(double a, double b) {
-    return a * b;
-  }
 }
